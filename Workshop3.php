cbae0a862380527b3537151e4250bd2f01f6f550
Amonpan2 <65160241@go.buu.ac.th>
2023-12-20 00:05:05 +0700

    Update Workshop3.php

---
+++ Workshop3.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <body>
+        <h1>ตารางสูตรคูณแม่ 2</h1>
+
+        <?php 
+        $numner = 2
+        
+        
+        // for($i=1; $i < 12; $i++)
+        // print_r($i  );
+        
+        ?>    
+         <?php $multi_x = 2; ?>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <?php
+                        print_r($numner);
+                        ?>
+                    </td>
+                    <td>x</td>
+                    <td>
+                        <?php
+                        print_r($num);
+                        ?>
+                    </td>
+                    <td>=</td>
+                    <td>
+                        <?php
+                        
+                        ?>
+                    </td>
+                    </tr>
+            </tbody>
+        </table>
+        <style>
+            table {
+                width: 150px;
+                height: 265px;
+            }
+        </style>
+       
+
+        </body>
+    </head>
+</html>
