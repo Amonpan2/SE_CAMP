418e021226e1b4e94787bcdf2983f214a561fc60
Amonpan2 <65160241@go.buu.ac.th>
2023-12-21 04:34:20 +0700

    test01

--- workshop3.php
+++ workshop3.php
@@ -1,7 +1,30 @@
 <html>
-    <head>
-        <body>
-            <h1>hello</h1>
-        </body>
-    </head>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
 </html>
