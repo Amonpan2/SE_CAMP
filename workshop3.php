62a684fc36576ce1e512dc46448fbb628bf241fa
Amonpan2 <65160241@go.buu.ac.th>
2023-12-23 01:26:30 +0700

    workshop3

--- workshop3.php
+++ workshop3.php
@@ -2,70 +2,97 @@
 
 <head>
     <meta charset="utf-8">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sarabun:wght@500;700&display=swap');
+    </style>
 </head>
 
 <body>
     <?php 
     $multi_x = 2;
-    $end = 1;
     ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-    <?php 
-//     for($i=1; $i <= 24; $i++){
-  //      print_r($i . "<br>");
-//
-  //   }
     
-    ?>
-    <table >
-        <tbody>
-            
-            <tr>
-                <td><?php 
-                for($i=1; $i <= 24; $i++){
-                    print_r($multi_x . "<br>");
-            
-                 }
-                 //print_r($multi_x . "<br>");
-                 ?></td>
-                <td>
-                <?php 
-                for($i=1 ; $i <=24 ; $i++){
-                    print_r(" x "."<br>");
-                }
-                ?>    
+    <div class="table">
+        <table >
+            <tbody>
                 
+                <tr>
+                    <td ><?php 
+                    for($i=1; $i <= 24; $i++){
+                        print_r($multi_x . "<br>");
                 
-                </td>
-                <td><?php
-                for($i=1 ; $i <=24 ; $i++){
-                    print_r($i ."<br>");
-                }
-                //print_r(1)
-                ?></td>
-                <td>
-                <?php 
-                for($i=1 ; $i <=24 ; $i++){
-                    print_r(" = "."<br>");
-                }
-                ?>    
-                
-                </td>
-                <td><?php 
-                for($i=1 ; $i <=24 ; $i++){
-                    print_r($multi_x*$i."<br>");
-                }
-                
-                //print_r($multi_x*$end)
-                
-                ?></td>
-            </tr>
-        </tbody>
-    </table>
+                    }
+                    //print_r($multi_x . "<br>");
+                    ?></td>
+                    <td>
+                    <?php 
+                    for($i=1 ; $i <=24 ; $i++){
+                        print_r(" x "."<br>");
+                    }
+                    ?>    
+                    
+                    
+                    </td>
+                    <td><?php
+                    for($i=1 ; $i <=24 ; $i++){
+                        print_r($i ."<br>");
+                    }
+                    //print_r(1)
+                    ?></td>
+                    <td>
+                    <?php 
+                    for($i=1 ; $i <=24 ; $i++){
+                        print_r(" = "."<br>");
+                    }
+                    ?>    
+                    
+                    </td>
+                    <td><?php 
+                    for($i=1 ; $i <=24 ; $i++){
+                        print_r($multi_x*$i."<br>");
+                    }
+                    
+                    //print_r($multi_x*$end)
+                    
+                    ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
     <style>
         table {
-            width: 150px;
-            height: 265px;
+            width: 340px;
+            height: 360px;
+            overflow-y: scroll;
+            background-color : lightpink; 
+           
+            margin: auto;
+            display: flex;
+            justify-content: center;
+            border-radius:25px; 
+            border : 2px solid white; 
+            
+          
+            
+        }
+        h1 {
+            text-align: center;
+            font-family: 'Sarabun', sans-serif;
+            
+        }
+        *{
+            background-color : hotpink; 
+            top : -5px;
+            
+            
+        }
+        td{
+            background-color : lightpink;
+        }
+        tbody {
+            background-color : lightpink;
         }
     </style>
 </body>
