92f87868a3219b2511eab53ac47533168e90553c
Amonpan2 <65160241@go.buu.ac.th>
2023-12-25 19:31:09 +0700

    update ralavel

---
+++ example-app/resources/views/myfolder/mypage.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+    </head>
+    <body>
+        <h1>My Folder And My Page</h1>
+        <h1><?php echo $var_a . ' ' . $var_b; ?></h1>
+        <h1>{{$var_a }} {{$var_b}}</h1>
+        <form method="post" action="/my-route">
+            @csrf
+            <input type="text" name = "myinput">
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+
+</html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\HttP\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //welcome.blade.php
+});
+Route::get('/my-route', function(){
+    // $data[''] = "";
+    $data = ['var_a' => 'Hello World'];
+    $data['var_b'] = "Laravel";
+    //echo view('myroute');
+    echo view('myfolder.mypage',$data); //เรียก viwe จาก folder
+
+});
+Route::post('/my-route', function(Request $req){
+    //แตกecho $req->input('/myinput');
+    $data['myinput'] = $req->input('myinput');
+    return view('myroute',$data);
 });
