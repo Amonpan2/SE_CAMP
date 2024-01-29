a2926593c29c5995868c2689d15a2bff6febd459
Amonpan2 <65160241@go.buu.ac.th>
2024-01-29 17:28:56 +0700

    titles

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -178,7 +178,7 @@
         }
 
     </style>
-    
+
 
     <!-- /.content -->
 @endsection
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\C_titles;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\MyController;
@@ -14,6 +14,7 @@ use App\Http\Controllers\MyController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('titles', C_titles::class);
 
 Route::get('/my-controller' , [MyController::Class, 'index']);
 /* การเรียกอีกแบบ*/
